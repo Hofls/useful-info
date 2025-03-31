a52ffa6b8e91681f57a726c1bd89c7ade0c0d513
hofls <hofls@ya.ru>
2025-03-31 23:58:54 +0500

    neutralino hello-world

---
+++ frontend/js-desktop-frameworks/neutralino/hello-world/README.md
@@ -0,0 +1,9 @@
+# Neutralino js
+* Install neutralino globally - `npm install -g @neutralinojs/neu`
+* Generate hello-world - `neu create hello-world`
+* Run app: 
+  ```
+  cd .\hello-world\
+  neu run
+  ```
+* Look at the console (lists files in current folder, reads first file)
---
+++ frontend/js-desktop-frameworks/neutralino/hello-world/resources/js/main.js
@@ -0,0 +1,24 @@
+
+function showInfo() {
+    document.getElementById('info').innerHTML = `
+        ${NL_APPID} is running on port ${NL_PORT} inside ${NL_OS}
+        <br/><br/>
+        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
+        `;
+}
+
+function openDocs() {
+    Neutralino.os.open("https://neutralino.js.org/docs");
+}
+
+function openTutorial() {
+    Neutralino.os.open("https://www.youtube.com/c/CodeZri");
+}
+
+function onWindowClose() {
+    Neutralino.app.exit();
+}
+
+Neutralino.init();
+Neutralino.events.on("windowClose", onWindowClose);
+showInfo();
